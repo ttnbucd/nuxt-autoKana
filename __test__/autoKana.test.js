34241c13d49e4b888a468463a6e93b78bd5352ee
ttnbucd <ttnbucd@gmail.com>
2022-08-11 12:41:55 +0900

    WIP

--- __test__/autoKana.test.js
+++ __test__/autoKana.test.js
@@ -9,7 +9,7 @@ test('init', () => {
   expect(typeof autokana).toBe('object');
 });
 
-test('input', () => {
+test('keydown event', () => {
   document.body.innerHTML = `
     <input name="name" id="name" value="">
     <input name="furigana" id="furigana" value="">
@@ -31,13 +31,51 @@ test('input', () => {
     { keyCode: 229, key: 'u', code: 'KeyU' }
   ];
 
-  const keyEvents = keyItems.map(keyItem => keyItem = new KeyboardEvent('keydown', keyItem));
+  const keyEvents = keyItems.map(item => item = new KeyboardEvent('keydown', item));
 
-  keyEvents.forEach(keyEvent => {
-    autokana.setKeyDownEvent(keyEvent)
+  keyEvents.forEach(e => {
+    autokana.setKeydownEvent(e)
   });
 
   target.value = '山田太郎';
 
   expect(autokana.getKanaValue()).toBe('ヤマダタロウ');
+});
+
+test('input event', () => {
+  global.window = Object.create(window);
+  Object.defineProperty(window, 'navigator', {
+    value: {
+      userAgent: 'iPhone'
+    }
+  });
+
+  document.body.innerHTML = `
+    <input name="name" id="name" value="">
+    <input name="furigana" id="furigana" value="">
+  `;
+  const autokana = new AutoKana('#name');
+  const target = document.getElementById('name');
+
+  const inputItems = [
+    { inputType: 'insertCompositionText', data: 'や' },
+    { inputType: 'insertCompositionText', data: 'ま' },
+    { inputType: 'insertCompositionText', data: 'た' },
+    { inputType: 'insertCompositionText', data: 'だ' },
+    { inputType: 'insertCompositionText', data: 'た' },
+    { inputType: 'insertCompositionText', data: 'ろ' },
+    { inputType: 'insertCompositionText', data: 'う' }
+  ];
+
+  const inputEvents = inputItems.map(item => item = new InputEvent('input', item));
+
+  inputEvents.forEach(e => {
+    autokana.setInputEvent(e)
+  });
+
+  console.log(autokana.inputNode);
+
+  target.value = '山田太郎';
+
+  expect(autokana.getKanaValue()).toBe('ヤマダタロウ');
 });
